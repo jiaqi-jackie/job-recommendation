6294e2fa2b73a9a10faac72f793afb10a329af47
jiaqi-jackie <jiaqili2001023@gmail.com>
2020-12-27 09:10:49 -0600

    test servlet passed

--- src/main/java/com/laioffer/job/servlet/TestServlet.java
+++ src/main/java/com/laioffer/job/servlet/TestServlet.java
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "TestServlet")
+@WebServlet(name = "TestServlet", urlPatterns = {"/test"})
 public class TestServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.getWriter().print("test passed");
     }
 }
