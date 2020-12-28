361d98130cdad3ede2c6bb06d26f903b5737a6a2
jiaqi-jackie <jiaqili2001023@gmail.com>
2020-12-27 19:49:00 -0600

    search

--- src/main/java/com/laioffer/job/servlet/SearchServlet.java
+++ src/main/java/com/laioffer/job/servlet/SearchServlet.java
@@ -1,6 +1,7 @@
 package com.laioffer.job.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import db.MySQLConnection;
 import entity.Item;
 import external.GitHubClient;
 
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 @WebServlet(name = "SearchServlet", urlPatterns = {"/search"})
 public class SearchServlet extends HttpServlet {
@@ -19,11 +21,18 @@ public class SearchServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userId = request.getParameter("user_id");
         double lat = Double.parseDouble(request.getParameter("lat"));
         double lon = Double.parseDouble(request.getParameter("lon"));
 
+        MySQLConnection connection = new MySQLConnection();
+        Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+        connection.close();
         GitHubClient client = new GitHubClient();
         List<Item> items = client.search(lat, lon, null);
+        for (Item item : items) {
+            item.setFavorite(favoritedItemIds.contains(item.getId()));
+        }
         ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(response.getWriter(), items);
     }
