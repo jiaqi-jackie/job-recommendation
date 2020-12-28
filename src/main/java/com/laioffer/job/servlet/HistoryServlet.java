2c4fdc20ac934b973416e014787b32811938a1b9
jiaqi-jackie <jiaqili2001023@gmail.com>
2020-12-27 19:47:18 -0600

    db

--- src/main/java/com/laioffer/job/servlet/HistoryServlet.java
+++ src/main/java/com/laioffer/job/servlet/HistoryServlet.java
@@ -3,6 +3,7 @@ package com.laioffer.job.servlet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import db.MySQLConnection;
 import entity.HistoryRequestBody;
+import entity.Item;
 import entity.ResultResponse;
 
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 @WebServlet(name = "HistoryServlet", urlPatterns = {"/history"})
 public class HistoryServlet extends HttpServlet {
@@ -27,7 +29,29 @@ public class HistoryServlet extends HttpServlet {
         mapper.writeValue(response.getWriter(), resultResponse);
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        ObjectMapper mapper = new ObjectMapper();
 
+        HistoryRequestBody body = mapper.readValue(request.getReader(), HistoryRequestBody.class);
+
+        MySQLConnection connection = new MySQLConnection();
+        connection.unsetFavoriteItems(body.userId, body.favorite.getId());
+        connection.close();
+
+        ResultResponse resultResponse = new ResultResponse("SUCCESS");
+        mapper.writeValue(response.getWriter(), resultResponse);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        ObjectMapper mapper = new ObjectMapper();
+
+        String userId = request.getParameter("user_id");
+
+        MySQLConnection connection = new MySQLConnection();
+        Set<Item> items = connection.getFavoriteItems(userId);
+        connection.close();
+        mapper.writeValue(response.getWriter(), items);
     }
 }
